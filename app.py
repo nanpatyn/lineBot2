8fad051cbf1e6690b7be1508b6ab872fce33a128
nanpatyn <56102833+nanpatyn@users.noreply.github.com>
2019-10-03 14:51:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def callback():
         sendText(user,'อาบน้ำ')
     else :
         sendText(user,'ว่าไงคะ')
-     return '',200
+    return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
